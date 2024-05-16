e78753a99497cab5152cc9692f34b5b52969031d
unknown <fred.skp2@gmail.com>
2024-05-16 12:39:31 +0100

    Added an API for Robots db

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, {Component} from 'react';
-import { robots } from './robots';
+// import { robots } from './robots';
 import CardList from './CardList';
 import SearchBox from './SearchBox';
     
@@ -9,10 +9,17 @@ class App extends Component {
     constructor() {
         super()
         this.state = {
-            robots: robots,
+            robots: [],
             searchfield: ''
         }
     }
+
+    componentDidMount(){
+        fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => {return response.json(); })
+        .then(users => {this.setState({robots: users}); })
+    }
+
     onSearchChange = (event) => {
        this.setState({ searchfield: event.target.value });      
     }
@@ -21,6 +28,9 @@ class App extends Component {
         const filteredRobots = this.state.robots.filter(robots =>{
             return robots.name.toLowerCase().includes(this.state.searchfield.toLowerCase()); //"includes = if includes, its a condition"
        }) ;
+       if (this.state.robots.length === 0){
+        return <h1 className='tc f1'>Loading</h1>
+       }else{
         return(
             <div className='tc'>
                 <h1 className='f1'>RoboFriends</h1>
@@ -29,6 +39,8 @@ class App extends Component {
             </div>
             
         );
+       }
+        
     }
 }
 
