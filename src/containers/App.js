7ba9bb3c6612368f9cb86a7ef53605e315e3ab78
unknown <fred.skp2@gmail.com>
2024-05-17 11:46:26 +0100

    Testing

--- src/containers/App.js
+++ src/containers/App.js
@@ -30,7 +30,7 @@ class App extends Component {
         const filteredRobots = robots.filter(robot =>{
             return robot.name.toLowerCase().includes(searchfield.toLowerCase()); //"includes = if includes, its a condition"
        }) ;
-       if (robots.length === 0){
+       if (robots.length === 0){ // or if(!robots.length), 0 = false, so if not 0 = if true
         return <h1 className='tc f1'>Loading</h1>
        }else{
         return(
